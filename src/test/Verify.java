632da3eae4e114ad2aea49418f8beb4e3dddee76
Ajay <ajay.thirumalesh@gmail.com>
2024-07-02 13:56:13 +0530

    first commit

---
+++ src/test/Verify.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Verify {
+
+	public static void main(String[] args) {
+		
+		System.out.println("My Name is Priya Racheal");
+		System.out.println("I am located in Bangalore, BTM Layout");
+		System.out.println("Test");
+
+	}
+	
+
+}
